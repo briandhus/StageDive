144fb203ce45b1e1a0f97d226ca076dc6983eb0f
jennie snider <jenniesnider@calvisitor-10-105-142-124.calvisitor.1918.berkeley.edu>
2017-07-07 08:37:23 -0700

    new js

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,12 +1,8 @@
 var artistInput;
 var locationInput;
-var userAirport = {};
-var eventAirport = {};
-var cheapestItinerary = {};
 var userLat;
 var userLng;
 var completeRecords = [];
-var sampleRecord = {};
 
 // Fix CORS issue
 var corsLink = "https://cors-anywhere.herokuapp.com/";
@@ -15,7 +11,7 @@ var corsLink = "https://cors-anywhere.herokuapp.com/";
 var artistInfoAPIKey = "a93674405c17c38cdbf92c8fcd42acea";
 
 // Sandbox Amadeus
-var sandboxAPIKey = "EolbHMC7fvkFAdvPmQNkJT8tApOIFKCu";
+var sandboxAPIKey = "9m6yYgeGs0WEqKpdcyFWPdo0yr2EIok4";
 
 //------- Google Maps & Autocomplete Form Setup -------
 var map;
@@ -24,7 +20,7 @@ var markers = []; // Necessary to remove markers
 
 function initMap() {
 	map = new google.maps.Map(document.getElementById("map"), {
-		zoom: 5,
+		zoom: 6,
 		center: new google.maps.LatLng(39.8097343, -98.5556199)
 	});
 	var addressInput = $("#address-input").val();
@@ -50,6 +46,7 @@ $(document).ready(function() {
 		deleteMarkers();
 
 		// Remove data from previous searches
+		completeRecords = [];
 		$("#artist-image").removeAttr("src");
 		$("#artist-bio").empty();
 		$(".search-results-table tr td").remove();
@@ -87,7 +84,7 @@ $(document).ready(function() {
 					// console.log("Longitude: " + response.results[0].geometry.location.lng);
 					userLat = response.results[0].geometry.location.lat;
 					userLng = response.results[0].geometry.location.lng;
-					airportNearUser(userLat, userLng);
+
 					// Reset map center to user input location
 					var center = new google.maps.LatLng(userLat, userLng);
 					map.setOptions({
@@ -132,8 +129,8 @@ $(document).ready(function() {
 			});
 
 			var bandsInTownQueryURL = corsLink + "https://rest.bandsintown.com/artists/" + artistInput + "/events?app_id=uc%20berkeley%20extension%20coding%20bootcamp";
-			console.log(bandsInTownQueryURL);
-    			$.ajax({
+			// console.log(bandsInTownQueryURL);
+    		$.ajax({
 				url: bandsInTownQueryURL,
 				method: "GET",
 				// dataType: "json"
@@ -154,7 +151,7 @@ $(document).ready(function() {
 					$(".search-results-table").append("<tr><td>Sorry, there are no upcoming events for " + artistInput + ". Please try again later.</td></tr>");
 				} else {
 					$.each(events, function(index, event) {
-						console.log(event);
+						// console.log(event);
 						var eventLat = parseFloat(event.venue.latitude);
 						var eventLng = parseFloat(event.venue.longitude);
 						var marker = new google.maps.Marker({
@@ -166,73 +163,57 @@ $(document).ready(function() {
 						});
 						markers.push(marker);
 
+						// Remove data from previous searches
+						cheapestItinerary = {};
+						eventAirport = {};
+
 						// If there is a purchasing link for the ticket, allows users to click on Venue to redirect to purchasing link
-						// var venueName = event.venue.name;
+						var venueName = event.venue.name;
 						var eventTicketURL = "";
 						if (event.offers.length > 0) {
 							eventTicketURL = event.offers[0].url;
 						};
 
-						// Create a sample record
-						sampleRecord = {
-							date: moment(event.datetime).format("MMMM D, YYYY"),
-							city: event.venue.city,
-							country: event.venue.country,
-							venue: event.venue.name,
-							eventTicketURL: eventTicketURL
-						};
-						// console.log(sampleRecord);
-
-						// If user inputs a location
-						if (locationInput) {
-							cheapestItinerary = {};
-							eventAirport = {};
-							airportNearEvent(eventLat, eventLng, sampleRecord);
-						} else {
+						if (!locationInput) {
 							var newRow = $("<tr>");
-							newRow.append($("<td>").text(sampleRecord.date));
-							newRow.append($("<td>").text(sampleRecord.city));
-							newRow.append($("<td>").text(sampleRecord.country));
+							newRow.append($("<td>").text(moment(event.datetime).format("MMMM D, YYYY")));
+							newRow.append($("<td>").text(event.venue.city));
+							newRow.append($("<td>").text(event.venue.country));
 							// If the eventTicketURL is not defined
-							if (sampleRecord.eventTicketURL == "") {
-								newRow.append($("<td>").text(sampleRecord.venue));
+							if (eventTicketURL === "") {
+								newRow.append($("<td>").text(event.venue.name));
 							} else {
-								newRow.append($("<td>").html("<a href=" + sampleRecord.eventTicketURL + ">" + sampleRecord.venue + "</a>"));
+								newRow.append($("<td>").html("<a href=" + eventTicketURL + ">" + event.venue.name + "</a>"));
 							};
 							$(".search-results-table").append(newRow);
+						} else {
+							completeRecords.push({
+								userLat: userLat,
+								userLng: userLng,
+								userAirport: {},
+								date: moment(event.datetime).format("MMMM D, YYYY"),
+								city: event.venue.city,
+								eventLat: eventLat,
+								eventLng: eventLng,
+								eventAirport: {},
+								country: event.venue.country,
+								venue: event.venue.name,
+								eventTicketURL: eventTicketURL,
+								allPossibleDates: [],
+								cheapestItinerary: {},
+								lowestAirFare: "",
+								lowestAirFareURL: "",
+							});
 						};
-
-						// // If user does not input a location
-						// if (!locationInput) {
-						// 	newRow.append(date);
-						// 	newRow.append(city);
-						// 	newRow.append(country);
-						// 	newRow.append(venue);
-						// 	$(".search-results-table").append(newRow);
-						// } else {
-						// 	cheapestItinerary = {};
-						// 	eventAirport = {};
-						// 	// if (airportNearUser === airportNearEvent) {
-						// 	// 	lowestAirFare = $("<td>").text("N/A");
-						// 	// };
-						// 	airportNearEvent(eventLat, eventLng, moment(event.datetime).format("YYYY-MM-DD"))
-						// // 	$.when(airportNearEvent(eventLat, eventLng, moment(event.datetime).format("YYYY-MM-DD"))).done(function() {
-						// // 		// debugger;
-						// // 		var cheapestDepartureDate = moment(cheapestItinerary.itineraries[0].outbound.flights[0].departs_at).format("YYYY-MM-DD");
-						// // 		var cheapestReturnDate = moment(cheapestItinerary.itineraries[0].inbound.flights[0].departs_at).format("YYYY-MM-DD");
-						// // 		var lowestAirFareURL = "https://www.google.com/flights/?#search;f=" + userAirport.airport + ";t=" + eventAirport.airport + ";d=" + cheapestDepartureDate + ";r=" + cheapestReturnDate + ";so=p";
-						// // 		var lowestAirFare = $("<td>").html("a href=" + lowestAirFareURL + ">" + cheapestItineraryPrice + "</a>");
-						// // 		newRow.append(date);
-						// // 		newRow.append(date);
-						// // 		newRow.append(city);
-						// // 		newRow.append(country);
-						// // 		newRow.append(venue);
-						// // 		newRow.append(lowestAirFare);
-						// // 		$(".search-results-table").append(newRow);
-						// // 	});
-						// };
 					});
 				};
+
+				// console.log(completeRecords);
+				$.each(completeRecords, function(index, record) {
+					// console.log(record);
+					airportNearUser(record);
+				});
+
 			});
 		} else {
 			$("#artist-input-error").show();
@@ -252,130 +233,160 @@ $(document).ready(function() {
 	});
 
 	// Find all the nearest airports within 500km of the user's latitude and longitude
-	var airportNearUser = function(lat, lng) {
-		var sandboxAirportNearQueryURL = "https://api.sandbox.amadeus.com/v1.2/airports/nearest-relevant?apikey=" + sandboxAPIKey + "&latitude=" + lat + "&longitude=" + lng;
+	var airportNearUser = function(record) {
+		var sandboxAirportNearQueryURL = "https://api.sandbox.amadeus.com/v1.2/airports/nearest-relevant?apikey=" + sandboxAPIKey + "&latitude=" + record.userLat + "&longitude=" + record.userLng;
 		// console.log(sandboxAirportNearQueryURL)
-		$.ajax({
+		$.when($.ajax({
 			url: sandboxAirportNearQueryURL,
 			method: "GET"
 		}).done(function(response) {
 			// console.log(response);
 			response.forEach(function(airport) {
-				if ($.isEmptyObject(userAirport) || userAirport.distance >= airport.distance) {
-						userAirport = airport;
+				if ($.isEmptyObject(record.userAirport) || record.userAirport.distance >= airport.distance) {
+						record.userAirport = airport;
+						// console.log(record.userAirport.airport);
 				};
 			});
-		});
+		})).then(function() {
+			console.log("found airport near user for current record");
+			airportNearEvent(record);
+		})
 	};
 
 	// Find all the nearest airports within 500km of the event's latitude and longitude
-	var airportNearEvent = function(lat, lng, sampleRecord) {
-		var sandboxAirportNearQueryURL = "https://api.sandbox.amadeus.com/v1.2/airports/nearest-relevant?apikey=" + sandboxAPIKey + "&latitude=" + lat + "&longitude=" + lng;
+	var airportNearEvent = function(record) {
+		var sandboxAirportNearQueryURL = "https://api.sandbox.amadeus.com/v1.2/airports/nearest-relevant?apikey=" + sandboxAPIKey + "&latitude=" + record.eventLat + "&longitude=" + record.eventLng;
 		// console.log(sandboxAirportNearQueryURL)
-		return $.ajax({
+		$.when($.ajax({
 			url: sandboxAirportNearQueryURL,
 			method: "GET"
 		}).done(function(response) {
 			// console.log(response);
 			response.forEach(function(airport) {
-				if ($.isEmptyObject(eventAirport) || eventAirport.distance >= airport.distance) {
-						eventAirport = airport;
+				if ($.isEmptyObject(record.eventAirport) || record.eventAirport.distance >= airport.distance) {
+						record.eventAirport = airport;
 				};
 			});
-			sampleRecord.userAirport = userAirport.airport;
-			sampleRecord.eventAirport = eventAirport.airport;
-			// console.log(sampleRecord);
-			cheapestAirFare(sampleRecord);
+		})).then(function() {
+			console.log("found airport near event for current record");
+			cheapestAirFare(record);
 		});
 	};
 
 
 	// Find the lowest air fare with departure date +/-3 days before venue date and return date +/- 3 days after venue date (9 possibilities)
-	var cheapestAirFare = function(sampleRecord) {
-		if (sampleRecord.userAirport === sampleRecord.eventAirport) {
+	var cheapestAirFare = function(record) {
+		if (record.userAirport.airport === record.eventAirport.airport) {
 			var newRow = $("<tr>");
-			newRow.append($("<td>").text(sampleRecord.date));
-			newRow.append($("<td>").text(sampleRecord.city));
-			newRow.append($("<td>").text(sampleRecord.country));
+			newRow.append($("<td>").text(record.date));
+			newRow.append($("<td>").text(record.city));
+			newRow.append($("<td>").text(record.country));
 			// If the ticketURL is not defined
-			if (!sampleRecord.ticketURL) {
-				newRow.append($("<td>").text(sampleRecord.venue));
+			if (record.eventTicketURL === "") {
+				newRow.append($("<td>").text(record.venue));
 			} else {
-				newRow.append($("<td>").html("<a href=" + sampleRecord.eventTicketURL + " target='_blank'>" + sampleRecord.venue + "</a>"));
+				newRow.append($("<td>").html("<a href=" + record.eventTicketURL + " target='_blank'>" + record.venue + "</a>"));
 			};
 			newRow.append($("<td>").text("Not Available"));
 			$(".search-results-table").append(newRow);
 		} else {
-			var eventDate = moment(sampleRecord.date).format("YYYY-MM-DD");
-			var allPossibleDates = [];
+			// var allPossibleDates = [];
 			var allPossibleDepartureDates = [];
 			var allPossibleReturnDates = [];
+			var currentDate = moment().format("YYYY-MM-DD");
 			for (var i = 1; i < 4; i++) {
-				allPossibleDepartureDates.push(moment(moment(eventDate).subtract(i, "days")).format("YYYY-MM-DD"));
-				allPossibleReturnDates.push(moment(moment(eventDate).add(i, "days")).format("YYYY-MM-DD"));
+				var possibleDepartureDate = moment(moment(record.date).subtract(i, "days")).format("YYYY-MM-DD");
+				// If the departure date is greater than the current date
+				if (moment(possibleDepartureDate).diff(moment(currentDate), "days") > 0) {
+					allPossibleDepartureDates.push(possibleDepartureDate);
+				};
+				allPossibleReturnDates.push(moment(moment(record.date).add(i, "days")).format("YYYY-MM-DD"));
 			};
-			console.log("All Possible Departure Dates: " + allPossibleDepartureDates);
-			console.log("All Possible Return Dates: " + allPossibleReturnDates);
+			// console.log("All Possible Departure Dates: " + allPossibleDepartureDates);
+			// console.log("All Possible Return Dates: " + allPossibleReturnDates);
 
-			allPossibleDepartureDates.forEach(function(departureDate) {
-				allPossibleReturnDates.forEach(function(returnDate) {
-					allPossibleDates.push({
-						departureDate: departureDate,
-						returnDate: returnDate
-					});
-				});
-			});
-
-			for (var i = 0; i < allPossibleDates.length; i++) {
-				console.log(i);
-				var possibleDate = allPossibleDates[i];
-				var numberOfResultsPerPair = 2;
-				var sandboxFlightLowFareQueryURL = "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=" + sandboxAPIKey + "&origin=" + sampleRecord.userAirport + "&destination=" + sampleRecord.eventAirport + "&departure_date=" + possibleDate.departureDate + "&return_date=" + possibleDate.returnDate + "&number_of_results=" + numberOfResultsPerPair;
-				console.log(sandboxFlightLowFareQueryURL);
-				// console.log("Departure Date: " + possibleDate.departureDate);
-				// console.log("Return Date: " + possibleDate.returnDate);
-				$.ajax({
-					url: sandboxFlightLowFareQueryURL,
-					method: "GET"
-				}).done(function(response) {
-					response.results.forEach(function(itinerary) {
-						if ($.isEmptyObject(cheapestItinerary) || parseFloat(cheapestItinerary.fare.price_per_adult.total_fare) > parseFloat(itinerary.fare.price_per_adult.total_fare)) {
-							cheapestItinerary = itinerary;
-							// console.log(cheapestItinerary);
-							sampleRecord.cheapestItinerary = cheapestItinerary;
-							var cheapestDepartureDate = moment(sampleRecord.cheapestItinerary.itineraries[0].outbound.flights[0].departs_at).format("YYYY-MM-DD");
-							var cheapestReturnDate = moment(sampleRecord.cheapestItinerary.itineraries[0].inbound.flights[0].departs_at).format("YYYY-MM-DD");
-							sampleRecord.lowestAirFare = parseFloat(cheapestItinerary.fare.price_per_adult.total_fare);
-							sampleRecord.lowestAirFareURL = "https://www.google.com/flights/?#search;f=" + sampleRecord.userAirport + ";t=" + sampleRecord.eventAirport + ";d=" + cheapestDepartureDate + ";r=" + cheapestReturnDate + ";so=p";
-							// console.log(sampleRecord);
-						};
-
-						if (i === allPossibleDates.length - 1) {
-							updateScreen(sampleRecord);
-						};
-					});
-				});
-			};
-
-		};
-	};
-
-	var updateScreen = function(sampleRecord) {
-		if (completeRecords.indexOf(sampleRecord) == -1) {
-			completeRecords.push(sampleRecord);
-			console.log(completeRecords);
-			var newRow = $("<tr>");
-			newRow.append($("<td>").text(sampleRecord.date));
-			newRow.append($("<td>").text(sampleRecord.city));
-			newRow.append($("<td>").text(sampleRecord.country));
-			// If the eventTicketURL is not defined
-			if (sampleRecord.eventTicketURL === "") {
-				newRow.append($("<td>").text(sampleRecord.venue));
+			// If there are no departure dates available 
+			if (allPossibleDepartureDates.length === 0) {
+				var newRow = $("<tr>");
+				newRow.append($("<td>").text(record.date));
+				newRow.append($("<td>").text(record.city));
+				newRow.append($("<td>").text(record.country));
+				// If the eventTicketURL is not defined
+				if (record.eventTicketURL === "") {
+					newRow.append($("<td>").text(record.venue));
+				} else {
+					newRow.append($("<td>").html("<a href=" + record.eventTicketURL + " target='_blank'>" + record.venue + "</a>"));
+				};
+				newRow.append($("<td>").text("Not Available"));
+				$(".search-results-table").append(newRow);
 			} else {
-				newRow.append($("<td>").html("<a href=" + sampleRecord.eventTicketURL + " target='_blank'>" + sampleRecord.venue + "</a>"));
+				$.each(allPossibleDepartureDates, function(index1, departureDate) {
+					$.each(allPossibleReturnDates, function(index2, returnDate) {
+						record.allPossibleDates.push({
+							departureDate: departureDate,
+							returnDate: returnDate
+						});
+					});
+				});
+
+				console.log("found all possible dates");
+				// console.log(record);
+
+				$.each(record.allPossibleDates, function(index, possibleDate) {
+					var numberOfResultsPerPair = 2;
+					var sandboxFlightLowFareQueryURL = corsLink + "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=" + sandboxAPIKey + "&origin=" + record.userAirport.airport + "&destination=" + record.eventAirport.airport + "&departure_date=" + possibleDate.departureDate + "&return_date=" + possibleDate.returnDate + "&number_of_results=" + numberOfResultsPerPair;
+					console.log(sandboxFlightLowFareQueryURL);
+					(function(index) {
+						$.ajax({
+							url: sandboxFlightLowFareQueryURL,
+							method: "GET",
+							contentType: "application/json; charset=utf-8",
+							dataType: "json",
+							cache: true
+						}).done(function(response) {
+							$.each(response.results, function(index, itinerary) {
+								// console.log("Departure Date: " + itinerary.itineraries[0].outbound.flights[0].departs_at);
+								// console.log("Return Date: " + itinerary.itineraries[0].inbound.flights[0].departs_at);
+								console.log("Price: " + itinerary.fare.price_per_adult.total_fare);
+								if ($.isEmptyObject(record.cheapestItinerary) || parseFloat(record.cheapestItinerary.fare.price_per_adult.total_fare) > parseFloat(itinerary.fare.price_per_adult.total_fare)) {
+									record.cheapestItinerary = itinerary;
+									console.log(record.cheapestItinerary);
+									var cheapestDepartureDate = moment(record.cheapestItinerary.itineraries[0].outbound.flights[0].departs_at).format("YYYY-MM-DD");
+									var cheapestReturnDate = moment(record.cheapestItinerary.itineraries[0].inbound.flights[0].departs_at).format("YYYY-MM-DD");
+									record.lowestAirFare = parseFloat(record.cheapestItinerary.fare.price_per_adult.total_fare);
+									record.lowestAirFareURL = "https://www.google.com/flights/?#search;f=" + record.userAirport.airport + ";t=" + record.eventAirport.airport + ";d=" + cheapestDepartureDate + ";r=" + cheapestReturnDate + ";so=p";
+								};
+							});
+							if (index === record.allPossibleDates.length - 1) {
+								var newRow = $("<tr>");
+								newRow.append($("<td>").text(record.date));
+								newRow.append($("<td>").text(record.city));
+								newRow.append($("<td>").text(record.country));
+								// If the eventTicketURL is not defined
+								if (record.eventTicketURL === "") {
+									newRow.append($("<td>").text(record.venue));
+								} else {
+									newRow.append($("<td>").html("<a href=" + record.eventTicketURL + " target='_blank'>" + record.venue + "</a>"));
+								};
+								// If the cheapestItinerary does not exist 
+								if (record.cheapestItinerary === "") {
+									newRow.append($("<td>").text("Not Available"));
+								} else {
+									newRow.append($("<td>").html("<a href=" + record.lowestAirFareURL + " target='_blank'>$" + record.lowestAirFare.toFixed(2)));
+								};
+								$(".search-results-table").append(newRow);
+								console.log("found cheapest fare");
+								console.log("done!!");
+							};
+						}).fail(function(jqXHR, textStatus) {
+							record.cheapestItinerary = "";
+							record.lowestAirFare = "";
+							record.lowestAirFareURL = "";
+						});
+					})(index);
+				});
 			};
-			newRow.append($("<td>").html("<a href=" + sampleRecord.lowestAirFareURL + " target='_blank'>$" + sampleRecord.lowestAirFare));
-			$(".search-results-table").append(newRow);
+
 		};
 	};
 
